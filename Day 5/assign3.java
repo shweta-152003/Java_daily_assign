224c0c4598aee2807cd63be2d7cab401bf825f6f
Shweta <shwetajagdale80@gmail.com>
2024-09-04 00:01:52 +0530

    Day5

---
+++ Day 5/assign3.java
@@ -0,0 +1,12 @@
+class Codex{
+	public static void main(String[] args){
+	char ch='c';
+	char ch1 ='b';
+ 	if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' 
+	   || ch=='A' || ch=='E' || ch=='I'|| ch=='O'||  ch=='U'){
+	  	 System.out.println( ch +" is vowel");
+          }else{
+		System.out.println( ch +" is consonant");		
+          }
+    }
+}
